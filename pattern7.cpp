39f32ab71e6eabf8916bdb6d2a4969916327e8fb
sm <sahilmalik15.sm@gmail.com>
2025-08-02 20:04:26 +0530

    pattern 6,7

---
+++ pattern7.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void print()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = 0; j < 2 * 5 - (2 * i + 1); j++)
+        {
+            cout << "*";
+        }
+
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    print();
+    return 0;
+}
